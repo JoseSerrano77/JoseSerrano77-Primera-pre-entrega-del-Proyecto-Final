30d5384ceeb26ff0fc58f23644b50df7fbc3b06a
JoseSerrano77 <jsrise77@gmail.com>
2023-01-21 09:00:42 -0300

    actualizacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import '../src/components/container/ItemListContainer/ItemListContainer.css'
-import '../src/components/NavBar/NavBar.css'
-import '../src/components/CartWidget/CartWidget.css'
-import NavBar from './components/NavBar/NavBar'
-import ItemListContainer from './components/container/ItemListContainer/ItemListContainer'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import "../src/components/container/ItemListContainer/ItemListContainer.css";
+import "../src/components/NavBar/NavBar.css";
+import "../src/components/CartWidget/CartWidget.css";
+import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/container/ItemListContainer/ItemListContainer";
 
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
- 
-  return(
-    <>
-   
-   
-    <NavBar/> 
-    <ItemListContainer saludo='Hola Coders Felices Fiestas!!!'/>
+  return (
     
+    <> 
+      <NavBar />
+      <ItemListContainer saludo="Cargando..." despedida="ULTIMA BIRRA" />
+       
     </>
-    // <ComponenteContenedor saludo='Hola Home' />  // 
-  )
+    // <ComponenteContenedor saludo='Hola Home' />  //
+  );
 }
 
-export default App
+export default App;
--- src/components/container/ItemListContainer/ItemListContainer.jsx
+++ src/components/container/ItemListContainer/ItemListContainer.jsx
@@ -1,14 +1,30 @@
 
 import Tarjetas from "../../Tarjetas/Tarjetas"
+import { useState, useEffect } from "react"
+
 
 const ItemListContainer= ({saludo})=> {
+const [contador, setContador]= useState(0) //hook estado de un componeete
+ 
+    const manejoContador = () => {
+        // contador= contador + 1
+        // console.log(contador)
+        setContador (contador + 1)
+    }
+   
+
     return(
-    
+    //   fragment <> </>
         <div className="promo">
      <h2>Envío Gratis desde $9999 solo CABA y zonas* de GBA + 3 cuotas sin interés: {saludo}</h2> 
- 
+    
+
      {/* <div>ItemListContainer</div> */}
      <Tarjetas/>
+        <p>Click  Nuevo:{contador}</p>
+     <button onClick={manejoContador}> sumar</button> 
+     {/* <button onClick= {() => setContador(contador +1)}> sumar</button>  */}
+     {/* con cada onClick me genera un nuevo re render o actualizacion*/}
      
     
 </div>
@@ -16,3 +32,5 @@ const ItemListContainer= ({saludo})=> {
     )
 }
 export default ItemListContainer
+
+// rendering : cuando se muestra el componente en el browser
